8f574730381f88d183e80a97173492e23b920547
timmytrumpit <timknott6@gmail.com>
2018-09-20 23:51:40 +1000

    Added Keys

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//dark sky secret key is:
+var key = 'fb82b8c51709279c70cbb84f2d2d1c2c';
+
+//Google Maps API key is:
+var mapKey = 'AIzaSyAQIYhE1X95IdCzO1bWw1pM8g-WAPJJ3K0'
+
+//Google Maps api url:
+var urlMap = 'https://www.google.com/maps/embed/v1/MODE?key=AIzaSyAQIYhE1X95IdCzO1bWw1pM8g-WAPJJ3K0&parameters'
 
 //location = canberra
 var liveLocation = '-35.184708, 149.132538';
